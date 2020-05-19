6cfefb671a41009d6972945fbef4c73c77187fb9
Naseer <imennkay@gmail.com>
2020-05-19 11:18:37 +0200

    changes

--- src/components/FirebaseConfig.js
+++ src/components/FirebaseConfig.js
@@ -4,13 +4,13 @@ import "firebase/auth";
 
 require('dotenv').config()
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APIKEY,
-    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASEURL,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-    appId: process.env.REACT_APP_APPID
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: "bookingapp-5c012",
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APP_ID
   };
 
   firebase.initializeApp(firebaseConfig);
